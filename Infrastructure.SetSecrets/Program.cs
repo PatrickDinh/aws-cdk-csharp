26645774b6c2ec91a39773d9272d2ead03a693c4
Patrick Dinh <d.conghoang@gmail.com>
2023-06-25 23:30:20 +1000

    Set secret local done

--- Infrastructure.SetSecrets/Program.cs
+++ Infrastructure.SetSecrets/Program.cs
@@ -1,4 +1,7 @@
-﻿using Amazon;
+﻿using System.Diagnostics;
+using Amazon;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
 using Amazon.SecretsManager;
 using Amazon.SecretsManager.Model;
 using Newtonsoft.Json;
@@ -10,7 +13,8 @@ var topSecretArn = cdkOutputs["AwsCdkCsharp-Secrets-Stack"]["TopSecretArn"];
 
 Console.WriteLine($"Setting secret for ARN {topSecretArn}");
 
-var client = new AmazonSecretsManagerClient(RegionEndpoint.APSoutheast2);
+var credentials = LoadSsoCredentials("sandbox");
+var client = new AmazonSecretsManagerClient(credentials, RegionEndpoint.APSoutheast2);
 await client.UpdateSecretAsync(new UpdateSecretRequest
 {
     SecretId = topSecretArn,
@@ -18,3 +22,29 @@ await client.UpdateSecretAsync(new UpdateSecretRequest
 });
 
 Console.WriteLine("Set secrets done");
+
+
+static AWSCredentials LoadSsoCredentials(string profile)
+{
+    var chain = new CredentialProfileStoreChain();
+    if (!chain.TryGetAWSCredentials(profile, out var credentials))
+        throw new Exception($"Failed to find the {profile} profile");
+
+    var ssoCredentials = credentials as SSOAWSCredentials;
+
+    ssoCredentials.Options.ClientName = "Set-Secrets-Local";
+    ssoCredentials.Options.SsoVerificationCallback = args =>
+    {
+        // Launch a browser window that prompts the SSO user to complete an SSO login.
+        //  This method is only invoked if the session doesn't already have a valid SSO token.
+        // NOTE: Process.Start might not support launching a browser on macOS or Linux. If not,
+        //       use an appropriate mechanism on those systems instead.
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = args.VerificationUriComplete,
+            UseShellExecute = true
+        });
+    };
+
+    return ssoCredentials;
+}
